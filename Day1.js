2b8b09807e614814e7aa0ab1572cfa2bd429f512
vinisha98 <96873814+vinisha98@users.noreply.github.com>
2022-03-22 00:46:25 +0530

    Added condition

--- Day1.js
+++ Day1.js
@@ -1,10 +1,33 @@
 console.log("hello world");
 console.log("Welcome to Employee Wage Computation Program on Master Branch");
 		const present=1;
-		let emp = Math.floor(Math.random() * 10) %  2;
-		if (emp == 	present){
-		console.log("Employee is present");
-		}
-		else{
-		console.log("Employee is absent");
-		}
+        	const parttime=2;
+        	const salaryperhour=20;
+        	var employeehours = 0;
+        	var total = 0;
+        	var days = 0;
+        	var workinghours = 0;
+       	 	while(days<20 && workinghours<100)
+       	 	{
+			let emp = Math.floor(Math.random() * 10) %  3;
+			switch(emp){
+            			case 1:
+              				employeehours=8;
+                			break;
+          	  		case 2:
+                			employeehours=4;
+                			break;
+            			case 3:
+                			employeehours=0;
+                			break;
+            			default:
+                			console.log("condition doesent exist")
+			}
+        		const dailywage = (salaryperhour*employeehours);
+        		total = (total + dailywage);
+        		days = (days+1);
+        		workinghours = (workinghours+employeehours);
+	    }
+        	console.log("Total salary will be ", total);
+        	console.log("Total working days will be ", days);       
+        	console.log("Total working hours will be ", workinghours);
