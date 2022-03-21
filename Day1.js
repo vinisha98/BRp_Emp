6dabe431064f8bfbd98b426afcd3bed882c3c55c
vinisha K R <vinishakr9@gmail.com>
2022-03-21 23:49:52 +0530

    employee attendance logic is added

---
+++ Day1.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+console.log("Welcome to Employee Wage Computation Program on Master Branch");
+		const present=1;
+		let emp = Math.floor(Math.random() * 10) %  2;
+		if (emp == 	present){
+		console.log("Employee is present");
+		}
+		else{
+		console.log("Employee is absent");
+		}
