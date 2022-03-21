c8daed392e1c39fdc2043370070ccef02e8ef822
vinisha98 <96873814+vinisha98@users.noreply.github.com>
2022-03-22 00:06:30 +0530

    Added daily wage calculation

--- Day1.js
+++ Day1.js
@@ -1,10 +1,16 @@
 console.log("hello world");
 console.log("Welcome to Employee Wage Computation Program on Master Branch");
 		const present=1;
+		const salaryperhour=20;
+        	var workinghours;
 		let emp = Math.floor(Math.random() * 10) %  2;
 		if (emp == 	present){
 		console.log("Employee is present");
+        	workinghours=8;
 		}
 		else{
 		console.log("Employee is absent");
+       		 workinghours=0;
 		}
+      		 const dailywage = (salaryperhour*workinghours);
+       		 console.log("salary will be ", dailywage);
