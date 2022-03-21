e911410fab738eabdf1691cfb51e2481e3d3e6db
vinisha98 <96873814+vinisha98@users.noreply.github.com>
2022-03-22 00:27:38 +0530

    added parttimer data

--- Day1.js
+++ Day1.js
@@ -5,21 +5,17 @@ console.log("Welcome to Employee Wage Computation Program on Master Branch");
 		const salaryperhour=20;
         	var workinghours;
 		let emp = Math.floor(Math.random() * 10) %  3;
-		switch(emp){
-            		case 1:
-	    	    		console.log("Employee is present");
-                		workinghours=8;
-                		break;
-            		case 2:
-                		console.log("Employee is parttimer");
-                		workinghours=4;
-                		break;
-            		case 3:
-	    	    		console.log("Employee is absent");
-                		workinghours=0;
-                		break;
-            		default:
-                		console.log("condition doesent exist")
+		if(emp == present){
+	    	    	console.log("Employee is present");
+                	workinghours=8;
+		}
+		else if (emp == parttime){
+			console.log("Employee is parttimer");
+                	workinghours=4;
+		}
+		else{
+			console.log("Employee is absent");
+                	workinghours=0;
 		}
         	const dailywage = (salaryperhour*workinghours);
        	 	console.log("salary will be ", dailywage);
